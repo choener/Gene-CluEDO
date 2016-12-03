524ae1dfae21fbac5d3b3dba83c9223042b67c62
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-12-03 02:17:28 +0100

    updating in prep for practical course

--- BioInf/HoxCluster.hs
+++ BioInf/HoxCluster.hs
@@ -7,6 +7,32 @@ import FormalLanguage
 
 
 
+-- A small grammar for Hamiltonian path problems. We need three rules due
+-- to normalization requirements for Inside-Outside.
+--
+-- Now, not a single node is set.
+--
+-- @
+-- X -> mpty <<< e
+-- @
+--
+-- A single node may be inserted, if the remainder of the set is then
+-- empty. This means that the @s@ terminal checks that only sets of size
+-- one are looked at.
+--
+-- @
+-- X -> node <<< s X
+-- @
+--
+-- An edge @k@ can be inserted, if at least one element in the set is still
+-- empty, and the set already contains at least one element.
+--
+-- @
+-- X -> edge <<< k X
+-- @
+--
+-- TODO generalize to be SHP and move into shortest path problem library
+
 [formalLanguage|
 Verbose
 Grammar: Hox
@@ -15,8 +41,11 @@ T: s
 T: k
 S: X
 X -> mpty <<< e     -- empty set
-X -> node <<< s     -- single node
+X -> node <<< s X   -- single node
 X -> edge <<< k X   -- edge k
+//
+Outside: Xoh
+Source: Hox
 //
 |]
 
