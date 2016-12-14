c7199b8dd06c825a266a6502a13e2fdcacc9ec4d
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-12-14 22:50:08 +0100

    implemented via new ADPfusion

--- BioInf/HoxCluster.hs
+++ BioInf/HoxCluster.hs
@@ -1,9 +1,12 @@
 
 module BioInf.HoxCluster where
 
-import ADP.Fusion.Core
-import Data.PrimitiveArray hiding (toList)
-import FormalLanguage
+import qualified Data.Vector.Fusion.Stream.Monadic as SM
+import           Numeric.Log
+
+import           ADP.Fusion.Core
+import           Data.PrimitiveArray hiding (toList)
+import           FormalLanguage
 
 
 
@@ -40,12 +43,106 @@ N: X
 T: s
 T: k
 S: X
-X -> mpty <<< e     -- empty set
+X -> mpty <<< ε     -- empty set
 X -> node <<< s X   -- single node
 X -> edge <<< k X   -- edge k
 //
 Outside: Xoh
 Source: Hox
 //
+Emit: Hox
+Emit: Xoh
 |]
+
+
+
+-- | maximal score algebra.
+--
+-- TODO The two Ints are the indices of the nodes and could be replaced?
+
+aMaxScore :: Monad m => SigHox m Double Double Int Int
+aMaxScore = SigHox
+  { hEdge = \edge x -> error "hEdge" + x
+  , hMpty = \() -> 0
+  , hNode = \node x -> error "hNode" + x
+  , hH    = SM.foldl' max (-999999)
+  }
+{-# Inline aMaxScore #-}
+
+aPretty :: Monad m => SigHox m String [String] Int Int
+aPretty = SigHox
+  { hEdge = \edge x -> show edge ++ "," ++ x
+  , hMpty = \()     -> ""
+  , hNode = \node x -> show node ++ x -- ok because it is the first node in the path
+  , hH    = SM.toList
+  }
+{-# Inline aPretty #-}
+
+aInside :: Monad m => SigHox m (Log Double) (Log Double) Int Int
+aInside = SigHox
+  { hEdge = \edge x -> error "hEdge" * x
+  , hMpty = \() -> 1
+  , hNode = \node x -> error "hNode" * x
+  , hH    = SM.foldl' (+) 0
+  }
+{-# Inline aInside #-}
+
+
+
+-- | Fill table for maximal scoring paths.
+
+runMaxScore :: ()
+runMaxScore = undefined
+{-# NoInline runMaxScore #-}
+
+
+
+-- | Attach backtracking for maximal scoring paths.
+
+runPretty :: ()
+runPretty = undefined
+{-# NoInline runPretty #-}
+
+
+
+-- | Inside scoring.
+
+runInside :: ()
+runInside = undefined
+{-# NoInline runInside #-}
+
+
+
+-- | Outside scoring.
+
+runOutside :: ()
+runOutside = undefined
+{-# NoInline runOutside #-}
+
+
+
+-- | Combine Inside and Outside to get the probability that a particular
+-- edge is taken.
+
+runEdgeProbabilities :: ()
+runEdgeProbabilities = undefined
+{-# NoInline runEdgeProbabilities #-}
+
+
+
+-- | Combine Inside and Outside to get the probability that a particular
+-- start node is being used.
+
+runStartProbabilities :: ()
+runStartProbabilities = undefined
+{-# NoInline runStartProbabilities #-}
+
+
+
+-- | Combine Inside and Outside to get the probability that a particular
+-- end node is being used.
+
+runEndProbabilities :: ()
+runEndProbabilities = undefined
+{-# NoInline runEndProbabilities #-}
 
--- BioInf/HoxCluster/ScoreMat.hs
+++ BioInf/HoxCluster/ScoreMat.hs
@@ -1,3 +1,8 @@
 
+-- | Import score matrices. These are type-isomorphic to Blast matrices.
+--
+-- TODO Generalize @Lib-BiobaseBlast@ to support importing matrices of
+-- these types.
+
 module BioInf.HoxCluster.ScoreMat where
 
