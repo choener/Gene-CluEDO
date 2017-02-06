939014d54afb44438f3a9756170747bbd09029ee
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-02-06 20:29:47 +0100

    disabled the empty properties

--- tests/properties.hs
+++ tests/properties.hs
@@ -4,7 +4,7 @@ module Main where
 -- | Each optimal path @a -> b -> c@ has has a co-optimal path @c -> b ->
 -- a@ whenever the distance matrix is symmetric.
 
-prop_symmetric_coopts :: ()
+-- prop_symmetric_coopts :: ()
 
 main :: IO ()
 main = return ()
