e0dd1193306185c7e9c9c15a47f6a73301b45f0c
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-02-20 17:39:51 +0100

    added command line helper

--- src/HoxCluster.hs
+++ src/HoxCluster.hs
@@ -18,11 +18,11 @@ data Options = Options
   deriving (Show,Data,Typeable)
 
 oOptions = Options
-  { infile      = def &= args
+  { infile      = def     &= args
   , outprefix   = Nothing &= help "prefix for the output files, calculated from input if not given"
-  , temperature = 0.01  &= help "lower temperatures favor the more optimal paths, defaults to 0.01"
-  , fillweight  = FWlog
-  , fillstyle   = FSfull
+  , temperature = 0.01    &= help "lower temperatures favor the more optimal paths, defaults to 0.01"
+  , fillweight  = FWlog   &= help "size of boxes: fwlog (log-scaled), fwlinear (linear scaled), fwfull (always full)"
+  , fillstyle   = FSfull  &= help "shading of boxes: fsopacitylog (log-scaled), fsopacitylinear (linear scaled), fsfull (always fully opaque)"
   }
 
 main :: IO ()
